2a38f5c201f1030d209c16a7471950db57156e4d
alirahmani <ali.rahmani@miare.ir>
2023-12-06 19:57:50 +0330

    feat(core): Generate field in admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from core.models import Product, Quote, Author, Landmark, Booking
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['slug', 'version', ]
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Quote)
 admin.site.register(Author)
 admin.site.register(Landmark)
