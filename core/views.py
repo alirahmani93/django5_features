3c523132ae42aa5b14937021b38b73b64c65cbea
alirahmani <ali.rahmani@miare.ir>
2023-12-09 15:18:04 +0330

    feat(core): python feature match-case vs if-statement => if is better than match

--- core/views.py
+++ core/views.py
@@ -1,3 +1,38 @@
+import random
+import time
+
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def check_if(repeat: int = 1000):
+    t0 = time.time()
+    for i in range(repeat):
+        a = random.choices(['x', 'y', 'z'])
+        z = 0
+        if a == 'x':
+            z += 1
+        elif a == 'y':
+            z += 2
+        elif a == 'z':
+            z **= z
+        else:
+            z -= 1
+    return time.time() - t0
+
+
+def check_match(repeat: int = 1000):
+    t0 = time.time()
+    for i in range(repeat):
+        z = 0
+        match random.choices(['x', 'y', 'z']):
+            case 'x':
+                z += 1
+            case 'y':
+                z += 2
+            case 'z':
+                z **= z
+            case _:
+                z -= 1
+    return time.time() - t0
